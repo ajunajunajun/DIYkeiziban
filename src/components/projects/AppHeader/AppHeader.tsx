bc3555b2802d0a4a99a24c5b3ae7ab4de9161c1e
ajunajunajun <ajunnajunn@gmail.com>
2019-09-03 00:06:20 +0900

    create base of header

---
+++ src/components/projects/AppHeader/AppHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import '../../../styles/projects/_header.scss'
+
+export default function AppHeader() {
+  return (
+    <header className="c-header">
+      <div className="c-header-inner">〇</div>
+    </header>
+  )
+}
