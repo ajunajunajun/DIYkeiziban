175349548926aad90698e3dd5b42dac2e445dfad
ajunajunajun <ajunnajunn@gmail.com>
2019-09-03 11:08:37 +0900

    use react-router-dom

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
 
 import '../styles/main.scss'
 import AppHeader from './projects/AppHeader/AppHeader'
 import IndexPage from './pages/IndexPage/IndexPage'
+import ArticlePage from './pages/ArticlePage/ArticlePage'
 
 export default () => (
-  <>
+  <BrowserRouter>
     <AppHeader />
-    <IndexPage />
-  </>
+    <Switch>
+      <Route path="/" exact={true} component={IndexPage} />
+      <Route path="/article" exact={true} component={ArticlePage} />
+    </Switch>
+  </BrowserRouter>
 )
