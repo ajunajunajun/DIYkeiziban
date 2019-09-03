771feb0ec7caf5db6d07c2100bbc4120dcf9f684
ajunajunajun <ajunnajunn@gmail.com>
2019-09-03 11:08:58 +0900

    create base of articlepage

---
+++ src/components/pages/ArticlePage/ArticlePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import '../../../styles/main.scss'
+import '../../../styles/projects/_home.scss'
+
+export default function ArticlePage() {
+  return (
+    <div className="c-home l-inner">
+      <h1>きじいちらｎ</h1>
+    </div>
+  )
+}
